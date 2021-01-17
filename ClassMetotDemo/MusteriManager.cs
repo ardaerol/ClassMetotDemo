fcae5df1014c5e8c190ec526fb4b7841cbdd0c27
Aziz Arda Erol <20190311036@dogus.edu.tr>
2021-01-17 16:16:26 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        Musteri musteri = new Musteri();
+       public static void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Yeni müşteri eklendi");
+            Console.WriteLine("-------------");
+        }
+
+        
+
+        public static void Listeleme(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Yasi);
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine("-------------");
+            }
+        }
+
+        public static void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi");
+            Console.WriteLine("-------------");
+        }
+
+}
+}
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Arda erol";
+            musteri1.Yasi = 20;
+            musteri1.Id = 1;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Eren alp";
+            musteri2.Yasi = 19;
+            musteri2.Id = 2;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Ahmet ark";
+            musteri3.Yasi = 21;
+            musteri3.Id = 3;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            MusteriManager.Ekle(musteri3);
+
+            MusteriManager.Listeleme(musteriler);
+            MusteriManager.Silme(musteri1);
+            MusteriManager.Listeleme(musteriler);
+
+
+        }
+    }
+}
